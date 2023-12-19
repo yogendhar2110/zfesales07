396cf660c47e4dfd333acc46730bc123813222f8
Yogendhar Velaga <yogendhar.velaga@ibm.com>
2023-12-19 03:50:57 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.ibm.zfesales07.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ zfe_ovp_07/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zfeovp07.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
